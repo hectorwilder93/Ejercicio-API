377d3f163d84b1c2172ae1d579d844dd7a6fe465
hector <cocorotero@hotmail.com>
2025-05-14 20:58:26 -0500

    Gracias

--- src/api.js
+++ src/api.js
@@ -3,13 +3,14 @@ const app = express();
 const port = 3000;
 
 const productRoutes=require('./routes/product.routes');
-const userRoutes=require('./routes/')
+const userroutes=require('./routes/user.routes')
 
 app.get('/api',(req, res)=>{
     res.send("Hola estamos en la cun")
 })
 
 app.use('/products',productRoutes)
+app.use('/login',userroutes)
 
 
 app.listen(port,()=>{
