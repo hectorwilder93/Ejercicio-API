a923f6329e54a9e6c8a3f64c3efe1009938f41a1
hector <cocorotero@hotmail.com>
2025-05-14 20:43:00 -0500

    Gracias

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -6,5 +6,8 @@ router.get('/create', (req, res)=>{
 router.get('/list', (req, res)=>{
     res.status(200).json({"message":"Esta es una lista de productos"});
 })
+router.get('/id', (req, res)=>{
+    res.status(200).json({"message":"Estos son los datos de un producto"});
+})
 
 module.exports = router; 
