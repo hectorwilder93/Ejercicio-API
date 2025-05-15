c43e69f9809190ad2ccb9c4a4f2a46c11132bb36
hector <cocorotero@hotmail.com>
2025-05-14 20:41:19 -0500

    Gracias

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,6 +1,10 @@
 const router= require("express").Router();
 router.get('/create', (req, res)=>{
     res.status(200).json({"message":"Estamos realizando una prueba"});
+});
+
+router.get('/list', (req, res)=>{
+    res.status(200).json({"message":"Esta es una lista de productos"});
 })
 
 module.exports = router; 
