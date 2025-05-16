afc08eece8f8939b70f599389475dbd550769c04
hector <cocorotero@hotmail.com>
2025-05-15 19:28:41 -0500

    update ahora

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require ('mongoose');
 const app = express();
 const port = 3000;
-require("dotenv");
+require("dotenv").config();
 
 const productRoutes=require('./routes/product.routes');
 const userroutes=require('./routes/user.routes');
